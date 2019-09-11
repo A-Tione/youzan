8e0813791a503bbafc14d1dcfbd9593e5065133f
heyang <heyanaggo@gmail.com>
2019-09-11 15:21:49 +0800

    首页商品列表mock数据

---
+++ src/modules/js/api.js
@@ -0,0 +1,16 @@
+let url = {
+  hotLists: '/index/hotLists'
+}
+
+//开发环境和正式环境的切换
+// let host = '';//正式
+let host = 'http://yapi.edaedu.net/mock/105';//开发
+
+for (let key in url) {
+  if (url.hasOwnProperty(key)) {//hasOwnProperty 表示对象自身属性中是否具有指定的属性
+    url[key] = host + url[key]
+  }
+}
+
+
+export default url
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,2 +1,35 @@
 import 'css/common.css'
 import './index.css'
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api'
+
+Vue.prototype.$ajax = axios;
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    list: null
+  },
+  created() {
+    this.getHotList()
+  },
+  methods:{
+    getHotList(){
+      this.$ajax({
+        method:'get',
+        url:url.hotLists,
+        // data:{pageNum: 1, pageSize: 6,}
+      }).then(res => {
+        console.log(res, 'res')
+        this.list = res.data.list
+        console.log(this.data.list,'11111111111111')
+      }).catch(err=>{
+        console.log(err,'err')
+      })
+    }
+  }
+})
+
+
+app.$mount('#app')
